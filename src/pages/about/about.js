e04941f5aff15d9a055529d50f0ffb051330bce7
Mxtgy <maxime.tanguy22@gmail.com>
2023-02-14 00:19:03 +0900

    Ajout Architecture et Router

---
+++ src/pages/about/about.js
@@ -0,0 +1,7 @@
+function About() {
+    return (
+        <p>I am the about page</p>
+    );
+}
+
+export default About;
---
+++ src/pages/logement/logement.js
@@ -0,0 +1,7 @@
+function Logement() {
+    return (
+        <p>I am the logement page</p>
+    );
+}
+
+export default Logement;
