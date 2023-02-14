bcaa6accc8494e2b10ee9e193849736a24a18928
Mxtgy <maxime.tanguy22@gmail.com>
2023-02-14 23:27:53 +0900

    Ajout header footer et data

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,12 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import logo from '../assets/logo.svg';
+import styles from './Header.module.css';
 
 function Header() {
     return (
-        <nav>
-            <p>I am the Header</p>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>A propos</NavLink>
-            <NavLink to='/logement'>Logement</NavLink>
+        <nav className={styles.nav}>
+            <Link className={styles.logo} to='/'>
+                <img src={logo} alt='Logo Kasa Header'></img>
+            </Link>
+            <div className={styles.links}>
+                <NavLink to='/' className={({ isActive }) => isActive ? styles.activeClassName : undefined }>Accueil</NavLink>
+                <NavLink to='/about' className={({ isActive }) => isActive ? styles.activeClassName : undefined }>À Propos</NavLink>
+            </div>
         </nav>
     );
 }
